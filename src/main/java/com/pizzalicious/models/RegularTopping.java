0f25103709905a6ea2a985bf2fe9b57beee1fa5b
Beimni406 <bkifle@my.yearupunited.org>
2025-11-12 13:44:26 -0500

    Added toString() method to display regular topping name and type

--- src/main/java/com/pizzalicious/models/RegularTopping.java
+++ src/main/java/com/pizzalicious/models/RegularTopping.java
@@ -3,14 +3,21 @@ package com.pizzalicious.models;
 // Represents regular toppings such as onions, spinach, olives, etc.
 public class RegularTopping extends Topping {
 
-    // Commit #1: base constructor
     public RegularTopping(String name) {
         // regular toppings are not premium and not extra by default
         super(name, false, false);
     }
 
-    // Commit #2: helper method to describe the topping type
     public String getTypeDescription() {
         return "Regular topping: " + name;
+    }
+
+    public void displayInfo() {
+        System.out.println("Added " + getTypeDescription());
+    }
+
+    @Override
+    public String toString() {
+        return name + " (Regular)";
     }
 }
