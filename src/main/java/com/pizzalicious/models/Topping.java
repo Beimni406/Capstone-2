665621d0437727a18e95707dbf6053c82a0c36fb
Beimni406 <bkifle@my.yearupunited.org>
2025-11-12 13:24:35 -0500

    Add constructor and getter methods for Topping class

--- src/main/java/com/pizzalicious/models/Topping.java
+++ src/main/java/com/pizzalicious/models/Topping.java
@@ -4,4 +4,24 @@ public abstract class Topping {
     protected String name;
     protected boolean isPremium;   // true if it's meat or cheese
     protected boolean isExtra;     // if customer adds extra portion
+
+    // Constructor
+    public Topping(String name, boolean isPremium, boolean isExtra) {
+        this.name = name;
+        this.isPremium = isPremium;
+        this.isExtra = isExtra;
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public boolean isPremium() {
+        return isPremium;
+    }
+
+    public boolean isExtra() {
+        return isExtra;
+    }
 }
