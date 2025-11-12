e204e2ae4374374a3c9ef8ac8ba187bb3473b1de
Beimni406 <bkifle@my.yearupunited.org>
2025-11-12 13:16:25 -0500

    Added constructor and basic getter methods to Pizza class

--- src/main/java/com/pizzalicious/models/Pizza.java
+++ src/main/java/com/pizzalicious/models/Pizza.java
@@ -3,8 +3,28 @@ package com.pizzalicious.models;
 import java.util.ArrayList;
 
 public abstract class Pizza {
+    // --- Commit #1 ---
     protected int size;            // 8, 12, or 16 inches
     protected String crust;        // thin, regular, thick, cauliflower
     protected boolean stuffedCrust;
     protected ArrayList<String> toppings = new ArrayList<>();
+
+    // --- Commit #2 ---
+    public Pizza(int size, String crust, boolean stuffedCrust) {
+        this.size = size;
+        this.crust = crust;
+        this.stuffedCrust = stuffedCrust;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getCrust() {
+        return crust;
+    }
+
+    public boolean isStuffedCrust() {
+        return stuffedCrust;
+    }
 }
