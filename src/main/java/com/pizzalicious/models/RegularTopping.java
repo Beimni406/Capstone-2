83d8bcba68d1b8e8a7575a8d457a1c726a73c979
Beimni406 <bkifle@my.yearupunited.org>
2025-11-12 13:41:08 -0500

    Added helper method getTypeDescription() and comments to RegularTopping class

--- src/main/java/com/pizzalicious/models/RegularTopping.java
+++ src/main/java/com/pizzalicious/models/RegularTopping.java
@@ -1,9 +1,16 @@
 package com.pizzalicious.models;
 
+// Represents regular toppings such as onions, spinach, olives, etc.
 public class RegularTopping extends Topping {
 
+    // Commit #1: base constructor
     public RegularTopping(String name) {
         // regular toppings are not premium and not extra by default
         super(name, false, false);
+    }
+
+    // Commit #2: helper method to describe the topping type
+    public String getTypeDescription() {
+        return "Regular topping: " + name;
     }
 }
