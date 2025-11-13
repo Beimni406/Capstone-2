f26792c0f9da46acf5f8aa718910495d67d40b75
Beimni406 <bkifle@my.yearupunited.org>
2025-11-13 09:00:42 -0500

    Added constructor and getter method to GarlicKnot class

--- src/main/java/com/pizzalicious/models/GarlicKnot.java
+++ src/main/java/com/pizzalicious/models/GarlicKnot.java
@@ -3,4 +3,14 @@ package com.pizzalicious.models;
 // Represents an order item for garlic knots
 public class GarlicKnot {
     private int quantity;   // number of garlic knot orders
+
+    // Constructor
+    public GarlicKnot(int quantity) {
+        this.quantity = quantity;
+    }
+
+    // Getter method
+    public int getQuantity() {
+        return quantity;
+    }
 }
