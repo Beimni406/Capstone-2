4da0f2aa7692fb4f623b261cab1e30c49815f924
Beimni406 <bkifle@my.yearupunited.org>
2025-11-13 09:15:06 -0500

    Added saveReceipt() method to write order details to text file

--- src/main/java/com/pizzalicious/utils/ReceiptFileManager.java
+++ src/main/java/com/pizzalicious/utils/ReceiptFileManager.java
@@ -2,8 +2,20 @@ package com.pizzalicious.utils;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import com.pizzalicious.models.Order;
 
 // Manages saving order receipts to a text file
 public class ReceiptFileManager {
 
+    // Saves an order receipt into a text file
+    public static void saveReceipt(Order order) {
+        try {
+            FileWriter writer = new FileWriter("receipt.txt");
+            writer.write(order.toString());
+            writer.close();
+            System.out.println("Receipt saved to receipt.txt");
+        } catch (IOException e) {
+            System.out.println("Error saving receipt: " + e.getMessage());
+        }
+    }
 }
