64c5612ffdadfc7e8241a274fde27867c20e4ef2
Beimni406 <bkifle@my.yearupunited.org>
2025-11-13 09:28:48 -0500

    Create Main class with welcome message

---
+++ src/main/java/com/pizzalicious/app/Main.java
@@ -0,0 +1,9 @@
+package com.pizzalicious.app;
+
+// Starting point of the Pizza-Licious app
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to PIZZA-licious!");
+    }
+}
